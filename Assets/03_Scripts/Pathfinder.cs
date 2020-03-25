6639d5b5f8ec24be7c32b16e4e5a40acbdb94aad
AssaSquid <jerome@nebula-tales.com>
2020-03-25 09:20:35 +0000

    Breadth First Search Algorithm added.
    Now exploring other neighbouring cubs and marking them as explored. Then moving to the next cube to use it as a new search center.

--- Assets/03_Scripts/Pathfinder.cs
+++ Assets/03_Scripts/Pathfinder.cs
@@ -7,12 +7,82 @@ public class Pathfinder : MonoBehaviour
     [SerializeField] Waypoint startWaypoint, endWaypoint;
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    [SerializeField] bool isRunning = true; // todo make private
+    
+    Vector2Int[] directions =   {
+                                    Vector2Int.up,
+                                    Vector2Int.right,
+                                    Vector2Int.down,
+                                    Vector2Int.left
+                                };
 
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
         ColorStartAndEnd();
+        Pathfind();
+        //ExploreNeighbours();
+    }
+
+    private void Pathfind()
+    {
+        queue.Enqueue(startWaypoint);
+
+        while (queue.Count > 0 && isRunning)
+        {
+            var searchCenter = queue.Dequeue();
+            print("Searching from : " + searchCenter); // todo remove log
+            HaltIfEndFound(searchCenter);
+            ExploreNeighbours(searchCenter);
+            searchCenter.isExplored = true;
+        }
+        // todo work-out path
+        print("Finished pathfinding ?");
+    }
+
+    private void HaltIfEndFound(Waypoint searchCenter)
+    {
+        if (searchCenter == endWaypoint)
+        {
+            print("Searching from end node, therefore stopping."); // todo remove log
+            isRunning = false;
+        }
+    }
+    
+    private void ExploreNeighbours(Waypoint from)
+    {
+        if (!isRunning) { return; }
+        
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int neighbourCoordinates = from.GetGridPos() + direction;
+            try
+            {
+                QueueNewNeighbours(neighbourCoordinates);
+            }
+            catch 
+            {
+            // do nothing
+            }
+        }
+    }
+
+    private void QueueNewNeighbours(Vector2Int neighbourCoordinates)
+    {
+        Waypoint neighbour = grid[neighbourCoordinates];
+        if (neighbour.isExplored)
+        { 
+            // do nothing
+        }
+        
+        else
+        {
+            neighbour.SetTopColor(Color.white); // todo move later
+            queue.Enqueue(neighbour);
+            print("Queueing " + neighbour);
+        }
     }
 
     private void ColorStartAndEnd()
--- Assets/03_Scripts/Waypoint.cs
+++ Assets/03_Scripts/Waypoint.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    public bool isExplored = false; // ok as is a data class
+    
     //Vector2Int gridPos; --> remove later if unused
     const int gridSize = 10;
 
@@ -21,8 +23,8 @@ public class Waypoint : MonoBehaviour
     public Vector2Int GetGridPos()
     {
         return new Vector2Int(
-            Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
-            Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+            Mathf.RoundToInt(transform.position.x / gridSize),
+            Mathf.RoundToInt(transform.position.z / gridSize)
         );
     }
 
