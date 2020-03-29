cc61caee9b19e5d6485bc35053fffe1fed3ffc35
AssaSquid <jerome@nebula-tales.com>
2020-03-29 19:52:34 +0100

    Towers can now shoot and destroy an enemy.
    Once the enmy is destroyed, the towers stop to shoot.

--- Assets/03_Scripts/EnemyDamage.cs
+++ Assets/03_Scripts/EnemyDamage.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
+    [SerializeField] int hitPoints = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     private void OnParticleCollision(GameObject other)
     {
-        print("I'm hit !");
+        ProcessHit();
+        if (hitPoints <= 0)
+        {
+            KillEnemy();
+        }
     }
+
+    void ProcessHit()
+    {
+        hitPoints = hitPoints - 1;
+        print("Current hitpoints are " + hitPoints);
+    }
+
+    void KillEnemy()
+    {
+        Destroy(gameObject);
+    }
+
 }
