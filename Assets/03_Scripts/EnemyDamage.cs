2446ce743311cee265da7089e3d4b53c08762c8e
AssaSquid <jerome@nebula-tales.com>
2020-03-29 20:52:32 +0100

    Path is only calculated once.
    A new enemy can be added without breaking the Pathfinder script.

--- Assets/03_Scripts/EnemyDamage.cs
+++ Assets/03_Scripts/EnemyDamage.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
-    [SerializeField] int hitPoints = 10;
+    [SerializeField] int hitPoints = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +24,6 @@ public class EnemyDamage : MonoBehaviour
     void ProcessHit()
     {
         hitPoints = hitPoints - 1;
-        print("Current hitpoints are " + hitPoints);
     }
 
     void KillEnemy()
--- Assets/03_Scripts/Pathfinder.cs
+++ Assets/03_Scripts/Pathfinder.cs
@@ -21,13 +21,21 @@ public class Pathfinder : MonoBehaviour
 
     public List<Waypoint> GetPath()
     {
+        if (path.Count == 0)
+        {
+            CalculatePath();
+        }
+        return path;
+    }
+
+    private void CalculatePath()
+    {
         LoadBlocks();
         ColorStartAndEnd();
         BreadthFirstSearch();
         CreatePath();
-        return path;
     }
-
+    
     private void CreatePath()
     {
         path.Add(endWaypoint);
