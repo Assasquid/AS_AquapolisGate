a6d3ee270fe4840fa0c59d27e8eeb31c1faf8c93
AssaSquid <jerome@nebula-tales.com>
2020-04-11 20:29:49 +0100

    Setting up coroutine for multiple enemy spawn.

---
+++ Assets/03_Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 3f;
+    [SerializeField] EnemyMovement enemyPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RepeatedlySpawnEnemies());
+    }
+
+    IEnumerator RepeatedlySpawnEnemies()
+    {
+        while (true) // forever WHY
+        {
+            // spawn an enemy
+            print("Spawning");
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+
+}
